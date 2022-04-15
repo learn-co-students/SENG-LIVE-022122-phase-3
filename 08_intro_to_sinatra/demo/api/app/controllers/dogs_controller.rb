201f3edfec8a9c8510fad5faebd4b47948310622
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-04-15 15:06:37 -0700

    Revert "finish demo portion of lecture"
    This reverts commit 5aaa97305f5711217c9894044c64f539f0a20c58.

--- 08_intro_to_sinatra/demo/api/app/controllers/dogs_controller.rb
+++ 08_intro_to_sinatra/demo/api/app/controllers/dogs_controller.rb
@@ -1,32 +1,6 @@
 class DogsController < ApplicationController
   
-  get "/dogs" do
-    Dog.all.to_json(methods: [:age])
-  end
-
-  get "/dogs/:id" do 
-    
-  end
-
-  post "/dogs" do 
-    # puts params
-    Dog.create(dog_params).to_json(methods: [:age])
-  end
-
-  patch "/dogs/:id" do
-    # binding.pry
-    dog = Dog.find_by_id(params[:id])
-    dog.update(dog_params)
-    dog.to_json(methods: [:age])
-  end
-
-  delete "/dogs/:id" do 
-    dog = Dog.find_by_id(params[:id])
-    dog.destroy
-    dog.to_json
-    # if you want to send back a no_content response instead, use the following line
-    # status 204 
-  end
+  
 
   private 
 
@@ -34,10 +8,7 @@ class DogsController < ApplicationController
   # we use this method to create a list of what's permitted to be passed to .create or .update
   # within controller actions.
   def dog_params
-    allowed_params = %w(name birthdate breed image_url)
-    params.filter do |param, value|
-      allowed_params.include?(param)
-    end
+    
   end
 
 end
