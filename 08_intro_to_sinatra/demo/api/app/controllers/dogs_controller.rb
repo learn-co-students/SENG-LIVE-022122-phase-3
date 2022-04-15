9cb5857b03cba7696c8bc16b8a86713d699b950d
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-04-15 10:58:11 -0700

    add lesson 8 intro to sinatra

---
+++ 08_intro_to_sinatra/demo/api/app/controllers/dogs_controller.rb
@@ -0,0 +1,16 @@
+class DogsController < ApplicationController
+  
+  get "/dogs" do 
+    Dog.all.to_json
+  end
+
+  private 
+
+  # a method used to specify which keys are allowed through params into the controller
+  # we use this method to create a list of what's permitted to be passed to .create or .update
+  # within controller actions.
+  def dog_params
+    
+  end
+
+end
